552d785abdc32afe632d4b35dac69f2f23579332
theguymat <42230087+theguymat@users.noreply.github.com>
2019-05-29 19:18:42 +1000

    Basic Camera Raycast
    Added raycast to camera and also tested the string.contains() code.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    RaycastHit hit;
+
+    Transform cam;
+
+    public LayerMask item;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    
+
+    void Update()
+    {
+
+        if (Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, 2f, item))
+        {
+            Debug.DrawRay(cam.position, cam.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            Debug.Log(hit.transform.gameObject.name);
+            if (hit.transform.gameObject.name.Contains("bar"))
+            {
+                print("YESSS!");
+            }
+        }
+        else
+        {
+            Debug.DrawRay(cam.position, cam.TransformDirection(Vector3.forward) * 2f, Color.green);
+        }
+
+    }
+}
